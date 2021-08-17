b60879eef8d62ee03d0ae28ed419646bfb1bae2c
Aleksandr Vinogradov <vsasha1305@gmail.com>
2021-08-17 20:06:55 +0300

    add body ip and time completion

--- server/swagger_server/controllers/server_controller.py
+++ server/swagger_server/controllers/server_controller.py
@@ -4,6 +4,8 @@ import six
 from swagger_server.models.server_addr import ServerAddr  # noqa: E501
 from swagger_server import util
 
+from datetime import *
+
 
 
 def server_delete_ip():  # noqa: E501
@@ -29,5 +31,11 @@ def server_post_ip(body=None):  # noqa: E501
     """
     if connexion.request.is_json:
         body = ServerAddr.from_dict(connexion.request.get_json())  # noqa: E501
+        print(type(body.ip))
+        print(type(body.time))
+        if body.ip is None:
+            body.ip = connexion.request.remote_addr
+        if body.time is None:
+            body.time = datetime.now()
         print(body)
     return {}, 201
