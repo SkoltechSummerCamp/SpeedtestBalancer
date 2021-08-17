5cd02af55448e35ecbbd0916a628f6dc534da359
Aleksandr Vinogradov <vsasha1305@gmail.com>
2021-08-17 17:20:00 +0300

    change controller

--- server/swagger_server/controllers/server_controller.py
+++ server/swagger_server/controllers/server_controller.py
@@ -13,7 +13,7 @@ def server_delete_ip():  # noqa: E501
 
     :rtype: List[ServerAddr]
     """
-    return 'do some magic!', 200
+    return 'do some magic!', 400
 
 
 def server_post_ip(body=None):  # noqa: E501
