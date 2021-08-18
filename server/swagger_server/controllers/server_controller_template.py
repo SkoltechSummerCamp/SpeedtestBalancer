4453f63ab1a8600e4ae7f218a9efe8fab66cf7fe
AleksandrVin <AleksandrVin@users.noreply.github.com>
2021-08-18 14:16:14 +0000

    Apply automatic changes

--- server/swagger_server/controllers/server_controller_template.py
+++ server/swagger_server/controllers/server_controller_template.py
@@ -1,6 +1,7 @@
 import connexion
 import six
 
+from swagger_server.models.inline_response200 import InlineResponse200  # noqa: E501
 from swagger_server.models.server_addr import ServerAddr  # noqa: E501
 from swagger_server import util
 
@@ -8,12 +9,12 @@ from swagger_server import util
 def server_delete_ip(body=None):  # noqa: E501
     """delete server IP
 
-    Send by server during shutdown # noqa: E501
+    Send by server during shutdown. # noqa: E501
 
     :param body: port of iperf server. Ip and time could be emply
     :type body: dict | bytes
 
-    :rtype: List[ServerAddr]
+    :rtype: List[InlineResponse200]
     """
     if connexion.request.is_json:
         body = ServerAddr.from_dict(connexion.request.get_json())  # noqa: E501
