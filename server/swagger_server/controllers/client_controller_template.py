9301944941e02759bd78341f26f1a2bdf9da3729
AleksandrVin <AleksandrVin@users.noreply.github.com>
2021-08-23 08:41:55 +0000

    Apply automatic changes

--- server/swagger_server/controllers/client_controller_template.py
+++ server/swagger_server/controllers/client_controller_template.py
@@ -5,7 +5,7 @@ from swagger_server.models.server_addr import ServerAddr  # noqa: E501
 from swagger_server import util
 
 
-def client_optain_ip():  # noqa: E501
+def client_obtain_ip():  # noqa: E501
     """obtain iperf server ip list to connect to
 
     Return servers ip list # noqa: E501
