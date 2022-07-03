ca9100ee71764b879e67d8a48cb823fb5139f352
Kazakov Mihail <advancerman@gmail.com>
2022-07-03 23:35:56 +0300

    Add swagger schema generation using drf-yasg

--- balancer/settings.py
+++ balancer/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.getenv('DEBUG') == 'True')
+DEBUG = (os.environ['DEBUG'] == 'True')
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split(',')
 
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'rest_framework',
     'services',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
