2f44d7ae5084cee3654d44bc2640134a7afdb1b3
Kazakov Mihail <advancerman@gmail.com>
2022-07-04 20:56:41 +0300

    Add tests

--- services/tests.py
+++ services/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class AcquireServiceTestCase(APITestCase):
+    def test_cant_acquire_service(self):
+        response = self.client.post(
+            path=reverse('service-acquirement'),
+            data={},
+            content_type='application/json',
+        )
+        self.assertEqual(response.status_code, status.HTTP_503_SERVICE_UNAVAILABLE)
